dcda5369fc569c86ec10f3c23d343c6a4aaf371a
joshmg88 <joshmg88@gmail.com>
2018-06-27 16:11:56 -0500

    initial

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const messagesCtrl = require("./controllers/messagesCtrl");
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post("/api/messages", messagesCtrl.create);
+app.get("/api/messages", messagesCtrl.read);
+app.put(`/api/messages/:id`, messagesCtrl.update);
+app.delete("/api/messages/:id", messagesCtrl.delete);
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
